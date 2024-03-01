e29729e651105b3384f0ec0bd662592e5c3b8323
Andrew_Tiurikov <andrew@realplusonline.com>
2024-03-01 20:14:55 +0100

    Add project files.

---
+++ MvcIntro/Controllers/ImageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcIntro.Models;
+
+namespace MvcIntro.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: ImageController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("ViewImage/{width}/{height}")]
+        public ActionResult Show(int width, int height)
+        {
+            var model = new ImageViewModel { Width = width, Height = height, Format = "jpg" };
+
+            return View(model);
+        }
+    }
+}
---
+++ MvcIntro/Models/ImageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcIntro.Models
+{
+    public class ImageViewModel
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Format { get; set; }
+    }
+}
